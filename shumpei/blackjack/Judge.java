013558874e6eee8185222ec7b2593d3d608bd275
Shumpei Fukui <fukui_shumpei@crossware.co.jp>
2020-09-30 10:34:13 +0900

    fix:戻り値をリテラルではなく定数にした。

--- shumpei/blackjack/Judge.java
+++ shumpei/blackjack/Judge.java
@@ -10,54 +10,56 @@ public class Judge {
      * @return 0（ディーラーの勝利）/1（引き分け）/2（プレーヤーの勝利）
      */
     public int handJudge(Hand playerHand, Hand dealerHand) {
-        int playerScore = playerHand.getScore();
-        int dealerScore = dealerHand.getScore();
-        int numOfPlayerCards = playerHand.getCardList().size();
-        int numOfDealerCards = dealerHand.getCardList().size();
+        final int dealerWin = 0;
+        final int tie = 1;
+        final int playerWin = 2;
+        final int playerScore = playerHand.getScore();
+        final int dealerScore = dealerHand.getScore();
+        final int numOfPlayerCards = playerHand.getCardList().size();
+        final int numOfDealerCards = dealerHand.getCardList().size();
 
         // 少なくともどちらかがバーストしている場合
-
-        if (playerScore > 21) {// プレイヤーがバーストしていたらディーラーの勝ち（ディーラーのバーストは関係ない）
-            return 0;
+        if (playerHand.checkBust()) {// プレイヤーがバーストしていたらディーラーの勝ち（ディーラーのバーストは関係ない）
+            return dealerWin;
         }
-
-        if (dealerScore > 21) {// ディーラーがバーストしていたら、プレイヤーの勝ち
-            return 2;
+        if (dealerHand.checkBust()) {// ディーラーがバーストしていたら、プレイヤーの勝ち
+            return playerWin;
         }
 
         // どちらもバーストせず、かつ同点でない場合
-
         if (dealerScore > playerScore) {// ディーラーがプレイヤーよりもスコアが高いなら、ディーラーの勝ち
-            return 0;
+            return dealerWin;
         }
-
         if (playerScore > dealerScore) {// プレイヤーがディーラーよりもスコアが高いなら、プレイヤーの勝ち
-            return 2;
+            return playerWin;
         }
 
         // どちらもバーストせず、かつ同点の場合
-
         if (dealerScore == 21 && numOfDealerCards == 2) {// どちらも21点だが、ディーラーの手札が2枚なら、ディーラーの勝ち（プレイヤーの手札数は関係ない）
-            return 0;
+            return dealerWin;
         }
-
         if (dealerScore == 21 && numOfPlayerCards == 2) {// どちらも21点だが、プレイヤーのみ手札が2枚なら、プレイヤーの勝ち
-            return 2;
+            return playerWin;
         }
 
-        return 1;// 上のいずれでもなければ引き分け
+        // 上のいずれでもなければ引き分け
+        return tie;
     }
 
     public int finalJudge(double playerPoint) {
+        final int loose = 0;
+        final int next = 1;
+        final int win = 2;
+
         if (playerPoint < 1) {
-            return 0;
+            return loose;
         }
 
         if (playerPoint >= 2000) {
-            return 2;
+            return win;
         }
 
-        return 1;
+        return next;
     }
 
 }
